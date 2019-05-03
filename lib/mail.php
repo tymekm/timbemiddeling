c9e3bef42aad7254e0cf21dfce52f41c8a38b860
tymekm <tymek.m@hotmail.com>
2019-05-03 12:38:32 +0200

    Fixed email

--- lib/mail.php
+++ lib/mail.php
@@ -1,36 +1,33 @@
 <?php
 require("PHPMailer_5.2.0/class.phpmailer.php");
 
-include '/home4/timbemid/etc/timbemiddeling.com/pass/sender-pass.php';
-$subject = $_REQUEST['name'];
+include '../../etc/timbemiddeling.com/pass/sender-pass.php';
+$name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
 $message = $_REQUEST['message'];
 
 $mail = new PHPMailer();
 
-$mail->IsSMTP();                                      // set mailer to use SMTP
-$mail->Host = "mail.timbemiddeling.com";  // specify main and backup server
-$mail->Port       = 587; 
-$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->IsSMTP(true);
+$mail->Host = "mail.timbemiddeling.com";
+$mail->Port = 587;
+$mail->SMTPAuth = true;
 $mail->SMTPSecure = "tls";
-$mail->Username = "sender@timbemiddeling.com";  // SMTP username
-$mail->Password = $pass; // SMTP password
+$mail->Username = "sender@timbemiddeling.com";
+$mail->Password = $pass;
+$mail->WordWrap = 50;
+$mail->IsHTML(true);
 
-$mail->From = "sender@timbemiddeling.com";
-
-$mail->FromName = $named;
-$mail->AddAddress("info@timbemiddeling.com");                  // name is optional
-
-$mail->WordWrap = 50;                                 // set word wrap to 50 characters
-$mail->IsHTML(true);                                  // set email format to HTML
-
-$mail->Subject = "Here is the subject";
-$mail->Body    = $message;
+$mail->From = $email;
+$mail->FromName = $name;
+$mail->AddAddress("info@timbemiddeling.com");
+$mail->Subject = $subject;
+$mail->Body = $message . '<br><br>Do not reply to this email';
 
 if(!$mail->Send())
 {
-   echo "Message could not be sent. 
-";
+   echo "Message could not be sent.";
    echo "Mailer Error: " . $mail->ErrorInfo;
    exit;
 }
